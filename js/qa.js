65029266fb0ccda66846fd02570967db69d1f57d
nebowork <carharttovich@gmail.com>
2026-03-18 18:56:04 +0300

    Update qa.js

--- js/qa.js
+++ js/qa.js
@@ -2,3 +2,19 @@ function helloQA() {
     console.log("Integration & QA ready ✅");
 }
 helloQA()
+
+<canvas id="gameCanvas" width="800" height="600" style="border:1px solid black;"></canvas>
+
+
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+function gameLoop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    // Здесь будут вызовы
+    
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
